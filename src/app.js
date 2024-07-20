514d4e4805b2e3403ba6dd2b59fbe11ed24ce09a
PabloGaston <47870194+PabloGaston@users.noreply.github.com>
2024-07-20 21:25:04 +0000

    Generador Excusas Terminado

--- src/app.js
+++ src/app.js
@@ -1,9 +1,35 @@
 /* eslint-disable */
 import "./style.css";
 
-window.onload = function() {
-  //write your code here
-  let randomNumber = Math.random() * 10;
-  document.querySelector("#the-excuse").innerHTML = randomNumber;
-  console.log("Hello Rigo from the console " + randomNumber);
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello Rigo from the console ");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["My", "Hers", "Your", "His"];
+  let subject = ["Jogger", "Bottle", "Dog", "Driver", "Comedian"];
+  let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
+  let possesion = ["homework", "toe", "car", "shoe"];
+  let where = ["on the street", "in my house", "in my driveway"];
+
+  let pronIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possIndex = Math.floor(Math.random() * possesion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possesion[possIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
