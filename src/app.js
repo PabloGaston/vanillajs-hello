13fda5b9c9e9fc68d74e08d5d487fd9563587784
PabloGaston <47870194+PabloGaston@users.noreply.github.com>
2024-07-20 19:39:14 +0000

    Generador de excusas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#the-excuse").innerHTML = randomNumber;
+  console.log("Hello Rigo from the console " + randomNumber);
 };
